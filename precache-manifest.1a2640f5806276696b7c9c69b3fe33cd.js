d807f62ddddc149856dc31b2f5a713ed109baec9
unknown <ifedayoagboola19@gmail.com>
2020-10-05 00:07:06 +0100

    Updates

---
+++ precache-manifest.1a2640f5806276696b7c9c69b3fe33cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afe073abe69f2fec78776a50e3df6e9",
+    "url": "/harry-potter/index.html"
+  },
+  {
+    "revision": "f549fc9804159ad1b919",
+    "url": "/harry-potter/static/css/main.26eed2cc.chunk.css"
+  },
+  {
+    "revision": "4ba7f1930a44e7f2719d",
+    "url": "/harry-potter/static/js/2.3b48f255.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/harry-potter/static/js/2.3b48f255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f549fc9804159ad1b919",
+    "url": "/harry-potter/static/js/main.411ff7ba.chunk.js"
+  },
+  {
+    "revision": "26a9c9700a247e11a8db",
+    "url": "/harry-potter/static/js/runtime-main.85cce344.js"
+  }
+]);
